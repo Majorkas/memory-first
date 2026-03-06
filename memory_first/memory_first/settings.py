23b7a8a707892c8585a3b54f1e9c876f8947ebcb
Aaron Richards <aaronrichards46@gmail.com>
2026-03-06 13:32:07 +0000

    made secret key secret

--- memory_first/memory_first/settings.py
+++ memory_first/memory_first/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kr)2s7j+cwumdx2biwry*a(gu%l0fd#lmguga9l06gt#0w)2s+'
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv('RENDER_EXTERNAL_HOSTNAME', '')]
 
 
 # Application definition
