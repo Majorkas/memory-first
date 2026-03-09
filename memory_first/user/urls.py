225f08bc28da2b7b52e2a60ac902e6abbbec2364
Aaron Richards <aaronrichards46@gmail.com>
2026-03-09 19:46:11 +0000

    added path for new template

--- memory_first/user/urls.py
+++ memory_first/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 from .views import CarerSignupView, PatientSignupView
 
 urlpatterns = [
+    path("accounts/signup/",TemplateView.as_view(template_name="account/signup_choice.html"),name="account_signup",),
     path("accounts/signup/carer/", CarerSignupView.as_view(), name="carer_signup"),
     path("accounts/signup/patient/", PatientSignupView.as_view(), name="patient_signup"),
     path("accounts/", include("allauth.urls")),
