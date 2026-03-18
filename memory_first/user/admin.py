f68dd98150e94d36ea880629d1e83232c6034184
Aaron Richards <aaronrichards46@gmail.com>
2026-03-18 14:55:21 +0000

    Added FamilyFriend model to admin

--- memory_first/user/admin.py
+++ memory_first/user/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import CUser, CarerProfile, PatientCarerRelationship, PatientProfile
+from .models import CUser, CarerProfile, PatientCarerRelationship, PatientProfile, FamilyFriend
 
 
 admin.site.register(CUser)
 admin.site.register(CarerProfile)
 admin.site.register(PatientCarerRelationship)
 admin.site.register(PatientProfile)
+admin.site.register(FamilyFriend)
