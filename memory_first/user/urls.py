d1972fa1f51e93279b9397f404c8712b4c0aed9f
Aaron Richards <aaronrichards46@gmail.com>
2026-03-18 15:01:44 +0000

    Added urls for the dashboard and patient detail view

--- memory_first/user/urls.py
+++ memory_first/user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
-from .views import CarerSignupView, PatientSignupView
+from .views import CarerSignupView, PatientSignupView, DashboardView, CarerPatientDetailView
 
 urlpatterns = [
     path("accounts/signup/",TemplateView.as_view(template_name="account/signup_choice.html"),name="account_signup",),
     path("accounts/signup/carer/", CarerSignupView.as_view(), name="carer_signup"),
     path("accounts/signup/patient/", PatientSignupView.as_view(), name="patient_signup"),
     path("accounts/", include("allauth.urls")),
+    path('dashboard/', DashboardView.as_view(), name="dashboard"),
+    path("dashboard/patient/<int:patient_id>/", CarerPatientDetailView.as_view(), name="carer_patient_detail"),
 ]
